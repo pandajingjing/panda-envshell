4887faad5793ac12abc8d419af3cd5077a378a53
jxu <root@sherry.jxu.home>
2016-09-13 13:45:31 +0800

    fix sshd configure

---
+++ assemble/sshd/2.0/configure.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#do some configure script
+
+info 'configure sshd start.'
+configure_bin $EXEC_DIR_ASSEMBLE_BIN_VERSION'/conf/sshd_config' '/etc/ssh/sshd_config' 
+info 'configure sshd success.'
+info 'make sure your public key has saved ~/.ssh/authorized_keys.'
--- assemble/sshd/config_sshd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-source ./common.sh
-
-info 'configure sshd start.'
-	configure_bin $EXEC_DIR_BINCONF/sshd/sshd_config /etc/ssh/sshd_config
-	/usr/sbin/sshd -t
-info 'configure sshd success.'
