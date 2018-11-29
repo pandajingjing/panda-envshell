db175edce978a31203dd0cd7a2e1214d0f3c71f7
徐晶 <xujing@jxulife.com>
2018-11-29 13:51:09 +0800

    5.2.4

--- assemble/bash/4.1.2/configure.sh
+++ assemble/bash/4.1.2/configure.sh
@@ -2,6 +2,6 @@
 
 #do some configure script
 
-info 'configure bash start.'
-configure_bin $EXEC_DIR_ASSEMBLE_BIN_VERSION'/conf/bashrc' '/etc/bashrc' '#custom bashrc start from here:' '#custom bashrc end here.'
-info 'configure bash successfully.'
+info 'configure profile start.'
+configure_bin $EXEC_DIR_ASSEMBLE_BIN_VERSION'/conf/profile' '/etc/profile' '#custom profile start from here:' '#custom profile end here.'
+info 'configure profile successfully.'
