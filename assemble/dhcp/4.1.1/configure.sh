8f9a88ee0f6b334dbd11a92c3e9f490065f2bc96
root <root@jxu.weihe>
2019-01-31 15:36:28 +0800

    V5.3

---
+++ assemble/dhcp/4.1.1/configure.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#do some configure script
+
+info 'configure dhcpd start.'
+    create_dir $INSTALL_DIR_LOG_BIN
+    configure_bin $EXEC_DIR_ASSEMBLE_BIN_VERSION'/conf/dhcpd.conf' '/etc/dhcp/dhcpd.conf'
+info 'configure dhcpd successfully.'
+info 'mod dhcpd service start.'
+    chkconfig --level 3 dhcpd on
+    chkconfig --level 3 dhcpd6 on
+    info "`chkconfig --list | grep dhcpd`"
+info 'mod dhcpd service successfully.'
---
+++ assemble/dhcp/4.1.1/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#do some install scripts
+
+info 'yum install dhcp start.'
+yum install dhcp -y
+info 'yum install dhcp successfully.'
