73ad27a84471035370c4fa2735926f10e4c10db8
徐晶 <xujing@jxulife.com>
2019-03-28 15:25:14 +0800

    temp save

--- assemble/redis/3.2.8/install.sh
+++ assemble/redis/3.2.8/install.sh
@@ -6,5 +6,5 @@ info 'compile and install redis start.'
     cd $EXEC_DIR_TEMP$BIN_CODE_DIR
     /usr/bin/make
     cd src
-    /bin/cp `/bin/ls -hl | /bin/grep 'rwx' | /bin/awk '{print $9}'` $INSTALL_DIR_BIN_BIN/
+    /bin/cp `/bin/ls -hl | /bin/grep 'rwx' | /usr/bin/awk '{print $9}'` $INSTALL_DIR_BIN_BIN/
 info 'compile and install redis successfully.'
